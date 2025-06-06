5f2520bf903d1999d043202028b4f36a57201677
Kenneth <Kennepo23@gmail.com>
2025-06-05 21:09:47 -0400

    added second component

--- BlessedBoxApp/app/baseStyles/baseStyles.tsx
+++ BlessedBoxApp/app/baseStyles/baseStyles.tsx
@@ -22,7 +22,7 @@ export const commonStyles = StyleSheet.create({
         fontFamily:'Playfair Display',
         fontSize:36,
         fontStyle:'normal',
-        fontWeight:'900',
+        fontWeight:900,
         lineHeight: 16 * 1.4
 },
     header:{
@@ -30,13 +30,13 @@ export const commonStyles = StyleSheet.create({
         fontFamily:'open-sans',
         fontSize:28,
         fontStyle:'normal',
-        fontWeight:700,
+        fontWeight: 700,
         lineHeight: 16 * 1.4
     },
     paragraph:{
         color:colors.dark_gray,
         fontFamily:'open-sans',
-        fontSize:26,
+        fontSize: 26,
         fontStyle:'normal',
         fontWeight:600,
         lineHeight: 16 * 1.4
@@ -53,26 +53,35 @@ export const commonStyles = StyleSheet.create({
         width: 'auto',
         height: 'auto',
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'center'
 },
     textContainer: {
         position: 'absolute',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'center'
 },
+// TODO:FIND A BETTER WAY OR ELIMINATE
     gridContainer: {
         flexDirection: 'row',
         alignItems: 'flex-start',
         columnGap: 16, 
-        marginTop: 20,
+        marginTop: 20
 },
     leftColumn: {
         flex: 1,
         justifyContent: 'flex-start',
-        rowGap:16,
+        rowGap:16
 },
     rightColumn: {
         justifyContent: 'center',
-        alignItems: 'center',
+        alignItems: 'center'
 },
+button:{
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    borderRadius:10,
+    backgroundColor:colors.dark_blue,
+    justifyContent: 'center',
+    alignItems: 'center'
+}
 });
--- BlessedBoxApp/app/index.tsx
+++ BlessedBoxApp/app/index.tsx
@@ -1,20 +1,31 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import { commonStyles } from "./baseStyles/baseStyles";
 import { colors } from "./baseStyles/colors";
 import { CircularProgress } from "./components/circularProgress";
 
 export default function Index() {
   return (
+    <View>
+
     <View style={[commonStyles.card, commonStyles.gridContainer]} >
+      <View style={commonStyles.leftColumn}>
+            <Text style={commonStyles.title}>OCC<Text style={{color:colors.red_label}}>2025</Text> </Text>
+            <Text style={commonStyles.header}>Yearly Goal</Text>
+            <Text style={commonStyles.paragraphBold}>Boxes Collected: <Text style={{color:colors.green}}>6,000</Text> </Text>
+            <Text style={commonStyles.paragraph}>Reaching Point: 12,000 Boxes</Text>
+      </View>
+          <View style={commonStyles.rightColumn}><CircularProgress percentage={50}/></View>
+    </View>
 
- <View style={commonStyles.leftColumn}>
-      <Text style={commonStyles.title}>OCC <Text style={{color:colors.red_label}}>2025</Text> </Text>
-      <Text style={commonStyles.header}>Yearly Goal</Text>
-      <Text style={commonStyles.paragraph}>Boxes Collected: <Text style={{color:colors.green}}> 6,000</Text> </Text>
-      <Text style={commonStyles.paragraph}>Reaching Point: 12,000 Boxes </Text>
- </View>
-    <View style={commonStyles.rightColumn}><CircularProgress percentage={50} /></View>
+    <View style={[commonStyles.card, {marginTop: 20, rowGap:16}]} > 
+            <Text style={commonStyles.paragraph}>Your Contribution</Text>
+            <Text style={commonStyles.paragraph}>You have deposited a total of<Text style={{color:colors.green}}> 45 </Text>boxes</Text>
+            <Text style={commonStyles.paragraph}>Great job! Keep it going!</Text>
+            <TouchableOpacity style={commonStyles.button} onPress={() => alert('Add more boxes!')}>
+              <Text style={[commonStyles.header, {color:colors.white}]}>View Activity</Text> </TouchableOpacity>
+    </View>
 
-  </View>
+    </View>
+
   );
 }
