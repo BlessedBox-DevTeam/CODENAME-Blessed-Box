e326c49ca9e7fdbb246f4bd4736e89700813b833
Kenneth <Kennepo23@gmail.com>
2025-09-06 21:03:32 -0400

    test on new branch

--- BlessedBoxApp/app/home/index.tsx
+++ BlessedBoxApp/app/home/index.tsx
@@ -1,48 +1,45 @@
-import React from "react";
-import { ScrollView, Text, TouchableOpacity, View } from "react-native";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import CircularProgress from "../components/circularProgress";
-import commonStyles from "../baseStyles/baseStyles";
-import colors from "../baseStyles/colors";
-import { Stack, useRouter } from "expo-router";
+import React from 'react';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import CircularProgress from '../components/circularProgress';
+import commonStyles from '../baseStyles/baseStyles';
+import colors from '../baseStyles/colors';
+import { Stack, useRouter } from 'expo-router';
 
-// FGM 
+// FGM
+//KWG probando
 
 export default function Index() {
   const router = useRouter();
   const handleQrCodePress = () => {
-    return router.replace("./qrCode");
+    return router.replace('./qrCode');
   };
   return (
     <SafeAreaProvider>
       <SafeAreaView
-        style={{ backgroundColor: colors.backgroundColor, flex: 1 }}
-      >
+        style={{ backgroundColor: colors.backgroundColor, flex: 1 }}>
         <ScrollView
-          style={{ flex: 1, paddingVertical: 0, paddingHorizontal: 20 }}
-        >
+          style={{ flex: 1, paddingVertical: 0, paddingHorizontal: 20 }}>
           <View style={[commonStyles.card, commonStyles.gridContainer]}>
             <View style={commonStyles.leftColumn}>
               <Text
                 style={[
                   commonStyles.paragraphExtraBold,
-                  { color: colors.green_label, gap: 0.5 }
-                ]}
-              >
-                OCC{" "}
+                  { color: colors.green_label, gap: 0.5 },
+                ]}>
+                OCC{' '}
                 <Text
                   style={[
                     commonStyles.paragraphExtraBold,
-                    { color: colors.red_label }
-                  ]}
-                >
+                    { color: colors.red_label },
+                  ]}>
                   2025
                 </Text>
               </Text>
               <Text style={commonStyles.header}>Yearly Goal</Text>
               <Text style={commonStyles.paragraph}>
-                Boxes Collected:{" "}
-                <Text style={commonStyles.paragraphExtraBold}>6,000</Text>{" "}
+                Boxes Collected:{' '}
+                <Text style={commonStyles.paragraphExtraBold}>6,000</Text>{' '}
               </Text>
               <Text style={commonStyles.paragraph}>
                 Reaching Point: 12,000 Boxes
@@ -60,21 +57,19 @@ export default function Index() {
             </Text>
             <View
               style={{
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center"
-              }}
-            >
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
               <View
                 style={{
                   backgroundColor: colors.green_label,
                   width: 50,
                   flex: 1,
-                  alignItems: "center",
+                  alignItems: 'center',
                   borderTopLeftRadius: 10,
-                  borderBottomLeftRadius: 10
-                }}
-              >
+                  borderBottomLeftRadius: 10,
+                }}>
                 <Text style={[commonStyles.header, { color: colors.white }]}>
                   15
                 </Text>
@@ -84,9 +79,8 @@ export default function Index() {
                   backgroundColor: colors.red_label,
                   width: 50,
                   flex: 1,
-                  alignItems: "center"
-                }}
-              >
+                  alignItems: 'center',
+                }}>
                 <Text style={[commonStyles.header, { color: colors.white }]}>
                   15
                 </Text>
@@ -96,14 +90,12 @@ export default function Index() {
                   backgroundColor: colors.light_gray,
                   width: 50,
                   flex: 1,
-                  alignItems: "center",
+                  alignItems: 'center',
                   borderTopRightRadius: 10,
-                  borderBottomRightRadius: 10
-                }}
-              >
+                  borderBottomRightRadius: 10,
+                }}>
                 <Text
-                  style={[commonStyles.header, { color: colors.dark_gray }]}
-                >
+                  style={[commonStyles.header, { color: colors.dark_gray }]}>
                   15
                 </Text>
               </View>
@@ -114,13 +106,11 @@ export default function Index() {
             <TouchableOpacity
               style={[
                 commonStyles.buttonNoShadow,
-                { backgroundColor: colors.dark_blue }
+                { backgroundColor: colors.dark_blue },
               ]}
-              onPress={() => alert("Add more boxes!")}
-            >
+              onPress={() => alert('Add more boxes!')}>
               <Text
-                style={[commonStyles.paragraphBold, { color: colors.white }]}
-              >
+                style={[commonStyles.paragraphBold, { color: colors.white }]}>
                 View Activity
               </Text>
             </TouchableOpacity>
@@ -128,55 +118,48 @@ export default function Index() {
           <View style={[commonStyles.card, { marginTop: 20, rowGap: 16 }]}>
             <Text style={commonStyles.header}>Your Church</Text>
             <Text style={[commonStyles.paragraph, { fontSize: 12 }]}>
-              Rejoice in the Lord always: and again I say, Rejoice.{" "}
+              Rejoice in the Lord always: and again I say, Rejoice.{' '}
               <Text style={[commonStyles.paragraph, { fontSize: 10 }]}>
-                (Philippians 4:4){" "}
+                (Philippians 4:4){' '}
               </Text>
             </Text>
             <Text style={commonStyles.paragraph}>
-              Iglesia Cristiana Bethlehem has collected{" "}
+              Iglesia Cristiana Bethlehem has collected{' '}
               <Text style={commonStyles.paragraphExtraBold}>10,000</Text> boxes
             </Text>
             <Text
               style={[
                 commonStyles.paragraphBold,
-                { color: colors.dark_blue, alignSelf: "flex-end" }
-              ]}
-            >
+                { color: colors.dark_blue, alignSelf: 'flex-end' },
+              ]}>
               See details
             </Text>
           </View>
         </ScrollView>
         <View
           style={{
-            flexDirection: "row",
-            alignItems: "center"
-          }}
-        >
+            flexDirection: 'row',
+            alignItems: 'center',
+          }}>
           <Text
-            style={[commonStyles.paragraph, { flex: 1, textAlign: "center" }]}
-          >
+            style={[commonStyles.paragraph, { flex: 1, textAlign: 'center' }]}>
             Dashboard
           </Text>
           <Text
-            style={[commonStyles.paragraph, { flex: 1, textAlign: "center" }]}
-          >
+            style={[commonStyles.paragraph, { flex: 1, textAlign: 'center' }]}>
             News
           </Text>
           <Text
-            style={[commonStyles.paragraph, { flex: 1, textAlign: "center" }]}
-            onPress={handleQrCodePress}
-          >
+            style={[commonStyles.paragraph, { flex: 1, textAlign: 'center' }]}
+            onPress={handleQrCodePress}>
             QR
           </Text>
           <Text
-            style={[commonStyles.paragraph, { flex: 1, textAlign: "center" }]}
-          >
+            style={[commonStyles.paragraph, { flex: 1, textAlign: 'center' }]}>
             History
           </Text>
           <Text
-            style={[commonStyles.paragraph, { flex: 1, textAlign: "center" }]}
-          >
+            style={[commonStyles.paragraph, { flex: 1, textAlign: 'center' }]}>
             Centers
           </Text>
         </View>
