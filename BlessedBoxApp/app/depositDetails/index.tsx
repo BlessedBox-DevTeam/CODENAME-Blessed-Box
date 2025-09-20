51e37c4e462eaf5bdc1162ee21b770556c8e57d4
Felix <eboozh23@gmail.com>
2025-09-20 19:43:03 -0400

    WIP Deposit Information

--- BlessedBoxApp/app/depositDetails/index.tsx
+++ BlessedBoxApp/app/depositDetails/index.tsx
@@ -1,6 +1,6 @@
 import { Stack, useRouter } from 'expo-router';
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import commonStyles from '../baseStyles/baseStyles';
 import SwitchSelector from 'react-native-switch-selector';
@@ -15,8 +15,99 @@ export default function Index() {
   const handleTab = (value: string) => {
     setActiveTab(value === 'information' ? 'information' : 'summary');
   };
+  const dummyData = {
+    fullName: 'John Doe',
+    email: 'john.doe@example.com',
+    transactionDate: 'November 10, 2025 - 3:00pm',
+    orderNumber: 'Order #12345',
+    churchText: 'Church',
+    churchName: 'Iglesia Cristiana Bethlehem',
+  };
+
+  const appendDepositDetails = () => {
+    return (
+      <View
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          flexDirection: 'column',
+          paddingVertical: 10,
+          borderColor: colors.light_gray,
+          // borderBottomWidth: index === parsedBoxLabels.length - 1 ? 0 : 1,
+          overflow: 'hidden',
+        }}>
+        {/* Name Container */}
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: 10,
+            alignItems: 'center',
+            borderBottomColor: colors.light_gray,
+            borderBottomWidth: 2,
+            paddingBottom: 10,
+            marginBottom: 10,
+          }}>
+          {/* SVG */}
+          <Text style={[commonStyles.paragraphBold, { color: colors.dark_blue }]}>{dummyData.fullName}</Text>
+        </View>
+
+        {/* Email Container */}
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: 10,
+            alignItems: 'center',
+            borderBottomColor: colors.light_gray,
+            borderBottomWidth: 2,
+            paddingBottom: 10,
+            marginBottom: 10,
+            marginTop: 2,
+          }}>
+          {/* SVG */}
+          <Text style={[commonStyles.paragraphBold, { color: colors.dark_blue }]}>{dummyData.email}</Text>
+        </View>
+
+        {/* DateContainer */}
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: 10,
+            alignItems: 'center',
+            borderBlockColor: colors.light_gray,
+            borderBottomWidth: 2,
+            paddingBottom: 10,
+            marginBottom: 10,
+          }}>
+          {/* SVG */}
+          <View>
+            <Text style={[commonStyles.paragraphBold, { color: colors.dark_blue }]}>{dummyData.transactionDate}</Text>
+            <Text style={[commonStyles.paragraph, { fontSize: 12 }]}>{dummyData.orderNumber}</Text>
+          </View>
+        </View>
+
+        {/* Church Container */}
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            gap: 10,
+            alignItems: 'center',
+          }}>
+          {/* SVG */}
+          <View>
+            <Text style={[commonStyles.paragraphBold, { color: colors.dark_blue }]}>{dummyData.churchText}</Text>
+            <Text style={[commonStyles.paragraph, { fontSize: 12 }]}>{dummyData.churchName}</Text>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       <Stack.Screen options={{ headerShown: false }} />
       <View
         style={{
@@ -38,6 +129,12 @@ export default function Index() {
           onPress={handleReturn}>{`${'Back'}`}</Text>
         <Text style={commonStyles.header}>Deposit Details</Text>
       </View>
+      <View style={[commonStyles.card, { marginVertical: 50, marginHorizontal: 100, alignItems: 'center' }]}>
+        <Text style={commonStyles.paragraph}>Logo</Text>
+      </View>
+      <Text style={[commonStyles.paragraphBold, { color: colors.dark_blue, alignSelf: 'center' }]}>
+        Requires Information
+      </Text>
       <SwitchSelector
         options={[
           { label: 'Deposit Information', value: 'information' },
@@ -49,6 +146,7 @@ export default function Index() {
         selectedColor={colors.white}
         buttonColor={colors.dark_green}
         borderColor={colors.white}
+        buttonMargin={4}
         style={{
           width: '100%',
           height: 50,
@@ -62,11 +160,34 @@ export default function Index() {
           fontFamily: commonStyles.paragraphBold.fontFamily,
         }}></SwitchSelector>
       <View style={commonStyles.card}>
-        {activeTab === 'information' ? (
-          <Text>Contenido de Información del Depósito</Text>
-        ) : (
-          <Text>Contenido de Resumen de la Caja jjjjj</Text>
-        )}
+        {activeTab === 'information' ? appendDepositDetails() : <Text>Contenido de Resumen de la Caja jjjjj</Text>}
+      </View>
+
+      <View style={{ marginTop: 'auto', padding: 20, width: '100%' }}>
+        <TouchableOpacity
+          style={[commonStyles.buttonNoShadow, { width: '100%' }]}
+          onPress={() => {
+            router.replace('./');
+          }}>
+          <Text style={[commonStyles.header, { color: colors.white }]}>Confirm Deposit</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={[
+            commonStyles.buttonNoShadow,
+            {
+              backgroundColor: colors.white,
+              borderColor: colors.red_label,
+              borderWidth: 2,
+              marginTop: 10,
+              width: '100%',
+            },
+          ]}
+          onPress={() => {
+            router.replace('./');
+          }}>
+          <Text style={[commonStyles.header, { color: colors.red_label }]}>Decline</Text>
+        </TouchableOpacity>
       </View>
     </SafeAreaView>
   );
