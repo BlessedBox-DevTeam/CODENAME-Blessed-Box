03fab81527827ac3e2c412a61f2054a6d151606f
Kenneth <Kennepo23@gmail.com>
2025-11-03 22:31:18 -0400

    wip on publishing app

--- BlessedBoxApp/app.config.js
+++ BlessedBoxApp/app.config.js
@@ -1,13 +1,60 @@
 import 'dotenv/config';
 
-export default ({ config }) => {
-  const APP_ENV = process.env.APP_ENV || 'development';
+const APP_ENV = process.env.APP_ENV || 'development';
 
-  return {
-    ...config,
-    extra: {
-      URL: APP_ENV === 'production' ? process.env.URL : process.env.URL,
-      PORT: APP_ENV === 'production' ? process.env.PORT : process.env.PORT,
+export default ({ config }) => ({
+  ...config,
+  name: 'BlessedBoxApp',
+  slug: 'BlessedBoxApp',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/images/icon.png',
+  scheme: 'blessedboxapp',
+  userInterfaceStyle: 'automatic',
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.kennepo23.blessedboxapp', // obligatorio
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/images/adaptive-icon.png',
+      backgroundColor: '#ffffff',
     },
-  };
-};
+    edgeToEdgeEnabled: true,
+    package: 'com.kennepo23.blessedboxapp', // obligatorio
+  },
+  web: {
+    bundler: 'metro',
+    output: 'static',
+    favicon: './assets/images/favicon.png',
+  },
+  plugins: [
+    'expo-router',
+    [
+      'expo-splash-screen',
+      {
+        image: './assets/images/splash-icon.png',
+        imageWidth: 200,
+        resizeMode: 'contain',
+        backgroundColor: '#ffffff',
+      },
+    ],
+  ],
+  experiments: { typedRoutes: true },
+  assetBundlePatterns: ['assets/fonts/*'],
+  extra: {
+    router: {},
+    URL: APP_ENV === 'production' ? process.env.URL : process.env.URL,
+    PORT: APP_ENV === 'production' ? process.env.PORT : process.env.PORT,
+    eas: {
+      projectId: 'c5861fad-066a-4c25-9240-ddabb311ac75', // obligatorio para EAS
+    },
+  },
+  updates: {
+    url: 'https://u.expo.dev/c5861fad-066a-4c25-9240-ddabb311ac75',
+  },
+  runtimeVersion: {
+    policy: 'appVersion',
+  },
+});
