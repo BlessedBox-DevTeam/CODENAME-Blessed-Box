cad48666d8ee1dba55870753960ba375d1ff85aa
Kenneth Williams <kennepo23@gmail.com>
2025-11-08 18:33:59 -0400

    aws build

---
+++ app.config.js
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+
+export default ({ config }) => {
+  const APP_ENV = process.env.APP_ENV || 'development';
+
+  return {
+    ...config,
+    owner: 'kennepo23',
+    name: 'blessedboxapp',
+    slug: 'blessedboxapp',
+    version: '1.0.0',
+    sdkVersion: '54.0.0',
+    platforms: ['android', 'ios', 'web'],
+    extra: {
+      APP_ENV,
+      URL: process.env.EXPO_PUBLIC_URL,
+      PORT: process.env.EXPO_PUBLIC_PORT,
+    },
+    updates: {
+      url: 'https://u.expo.dev/6ab4cd27-c46d-4cc2-be7e-ebf6a3471209',
+    },
+    runtimeVersion: {
+      policy: 'sdkVersion',
+    },
+    android: {
+      package: 'com.kennepo23.blessedboxapp',
+      versionCode: 1,
+    },
+    ios: {
+      bundleIdentifier: 'com.kennepo23.blessedboxapp',
+      buildNumber: '1.0.0',
+    },
+  };
+};
--- app/(public)/login.tsx
+++ app/(public)/login.tsx
@@ -15,8 +15,6 @@ const extra = Constants.expoConfig?.extra;
 const API_URL = extra?.URL;
 const API_PORT = extra?.PORT;
 
-console.log(extra);
-
 export default function Index() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
