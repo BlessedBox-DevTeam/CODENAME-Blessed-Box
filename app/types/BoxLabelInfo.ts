60d2e7dc4a11baed3dad25ab5c917d9929a697d7
Kenneth Williams <kennepo23@gmail.com>
2025-11-13 09:50:45 -0400

    deleted from dev

--- app/types/BoxLabelInfo.ts
+++
@@ -1,9 +0,0 @@
-export type BoxLabelInfo = {
-  selectedAge: '2-4' | '5-9' | '10-14' | false;
-  quantity: number;
-  gender: number | false;
-};
-
-export type BoxLabelType = {
-  getData: () => BoxLabelInfo;
-};
