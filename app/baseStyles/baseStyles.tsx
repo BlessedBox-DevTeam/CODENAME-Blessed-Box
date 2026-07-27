71788c093c62b9c01f29fd49c6682db094d97cf8
Felix <eboozh23@gmail.com>
2026-07-26 21:01:46 -0400

    test from front end

--- app/baseStyles/baseStyles.tsx
+++ app/baseStyles/baseStyles.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native'; // Hello from front
 import colors from './colors';
 const commonStyles = StyleSheet.create({
   card: {
